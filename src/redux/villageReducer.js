6f1e64b37ac6ff559527e8b2d02716b27a3b4099
BenDehlin <ben.dehlin@gmail.com>
2020-04-29 22:05:30 -0600

    village reducer

---
+++ src/redux/villageReducer.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import {SET_VILLAGE, SET_VILLAGES} from './actionTypes'
+
+const initialState = {
+    village: {
+        village_id: '',
+        village_name: '',
+        x_coord: null,
+        y_coord: null
+    },
+    villages: []
+}
+
+export function setVillage(payload){
+    return {type: SET_VILLAGE, payload}
+}
+
+export function setVillages(payload){
+    return {type: SET_VILLAGES, payload}
+}
+
+export default function villageReducer(state = initialState, action){
+    const {type, payload} = action
+    switch(type){
+        case SET_VILLAGE:
+            return {...state, village: payload}
+        case SET_VILLAGES:
+            return {...state, villages: payload}
+        default:
+            return state
+    }
+}
