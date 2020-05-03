e2f8617d68b4d32f5c326851ee4ced5a349be18a
BenDehlin <ben.dehlin@gmail.com>
2020-05-02 20:37:41 -0600

    update massive connection

--- server/index.js
+++ server/index.js
@@ -1,33 +1,41 @@
-require('dotenv').config({path: __dirname + '/../.env'})
-const express= require('express');
-const session= require('express-session');
-const massive= require('massive');
-const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING}= process.env
-const app= express()
+require("dotenv").config({ path: __dirname + "/../.env" })
+const express = require("express")
+const session = require("express-session")
+const massive = require("massive")
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
+const app = express()
 
 app.use(express.json())
-app.use(session({
+app.use(
+  session({
     resave: false,
     saveUninitialized: true,
     secret: SESSION_SECRET,
-    cookie: {maxAge: 1000 * 60 *60}
-}))
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
+)
 
-massive(CONNECTION_STRING).then(db => {
-    app.set('db', db)
-    console.log('database is building its tribe')
-    app.listen(SERVER_PORT, () => console.log(`server is raiding tribes at port ${SERVER_PORT}`))  
-}).catch(err => console.log(err))
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+})
+  .then((db) => {
+    app.set("db", db)
+    console.log("database is building its tribe")
+    app.listen(SERVER_PORT, () =>
+      console.log(`server is raiding tribes at port ${SERVER_PORT}`)
+    )
+  })
+  .catch((err) => console.log(err))
 
 //Controllers
-const authCtrl = require('./controllers/authController')
-
+const authCtrl = require("./controllers/authController")
 
 //ENDPOINTS
 
 //auth endpoints
 
-app.get('/api/user', authCtrl.getUser)
-app.post('/auth/login', authCtrl.login)
-app.post('/auth/register', authCtrl.register)
-app.post('/auth/logout', authCtrl.logout)
+app.get("/api/user", authCtrl.getUser)
+app.post("/auth/login", authCtrl.login)
+app.post("/auth/register", authCtrl.register)
+app.post("/auth/logout", authCtrl.logout)
