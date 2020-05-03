c36da7159557ebc795462c4283ec21722d468fb4
BenDehlin <ben.dehlin@gmail.com>
2020-05-02 21:35:01 -0600

    login and register pages

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,51 +1,47 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs")
 
 module.exports = {
-    register: async(req, res) => {
-        const{username, password, email} = req.body;
-        const db = req.app.get('db')
-        
-        let user = await db.auth.login(username);
-        if(user[0]){
-            return res.status(400).send('User Already Exists');
+  register: async (req, res) => {
+    const { username, password, email } = req.body
+    const db = req.app.get("db")
 
-        }
-        let salt = bcrypt.genSaltSync(10);
-        let hash = bcrypt.hashSync(password, salt);
-        let newUser = await db.auth.register(username, hash, email);
-        
-        req.session.user = newUser[0];
-        res.status(201).send(req.session.user);
-
-    },
-
-    login: async(req, res) => {
-        const {username, password} = req.body;
-        const db = req.app.get('db');
-
-        let user = await db.auth.login(username);
-        if(!user[0]){
-            return res.status(400).send('Username not found');
-
-        }
-        let authenticated = bcrypt.compareSync(password, user[0].password);
-        if(!authenticated){
-            return res.status(401).send('Password is incorrect, try again')
-        }
-        delete user[0].password;
-        req.session.user = user[0];
-        res.status(202).send(req.session.user)
-    },
-    logout: (req, res) => {
-        req.session.destroy();
-        res.sendStatus(200)
-    },
-    getUser: (req, res) => {
-        if(req.session.user){
-            res.status(200).send(req.session.user);
-
-        }else{
-            res.status(200).send('no user on session')
-        }
+    let user = await db.auth.check_user(username)
+    if (user[0]) {
+      return res.status(400).send("User Already Exists")
     }
+    let salt = bcrypt.genSaltSync(10)
+    let hash = bcrypt.hashSync(password, salt)
+    let newUser = await db.auth.register_user(username, hash, email)
+
+    req.session.user = newUser[0]
+    res.status(201).send(req.session.user)
+  },
+
+  login: async (req, res) => {
+    const { username, password } = req.body
+    const db = req.app.get("db")
+
+    let user = await db.auth.check_user(username)
+    if (!user[0]) {
+      return res.status(400).send("Username not found")
+    }
+    let authenticated = bcrypt.compareSync(password, user[0].password)
+    if (!authenticated) {
+      return res.status(401).send("Password is incorrect, try again")
+    }
+    delete user[0].password
+    req.session.user = user[0]
+    res.status(202).send(req.session.user)
+  },
+  logout: (req, res) => {
+    req.session.destroy()
+    res.sendStatus(200)
+  },
+  getUser: (req, res) => {
+    if (req.session.user) {
+      res.status(200).send(req.session.user)
+    } else {
+      res.status(200).send("no user on session")
+    }
+  },
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react"
+import useInput from "../hooks/useInput"
+import { useHistory } from "react-router-dom"
+import { setUser } from "../redux/authReducer"
+import { connect, useDispatch } from "react-redux"
+import axios from "axios"
 
-const Login = (props) => {
-    return (
-        <div>Login</div>
-    )
+const Login = ({ setUser }) => {
+  const [{ email, password }, { setInput, resetInputs }] = useInput({
+    email: "",
+    password: "",
+  })
+  const { push } = useHistory()
+  //   const dispatch = useDispatch()
+  const login = (e) => {
+    e.preventDefault()
+    axios.post("/auth/login", { email, password }).then((results) => {
+      setUser(results.data)
+      console.log(email, password)
+      resetInputs()
+      push("/dashboard")
+    })
+  }
+  return (
+    <div>
+      <div>Login</div>
+      <form onSubmit={login}>
+        <input
+          name="email"
+          value={email}
+          placeholder="enter email"
+          onChange={setInput}
+        />
+        <input
+          name="password"
+          value={password}
+          placeholder="enter password"
+          onChange={setInput}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  )
 }
 
-export default Login
+export default connect(null, { setUser })(Login)
