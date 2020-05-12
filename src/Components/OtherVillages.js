c5e02bb8cae42dc56b645fc96f06f2c552bc23df
BenDehlin <ben.dehlin@gmail.com>
2020-05-11 18:18:40 -0600

    added viewing other villages, uses history.push to push between other villages and your village. worth considering refactor down the line.

--- src/Components/OtherVillages.js
+++ src/Components/OtherVillages.js
@@ -1,8 +1,10 @@
 import React from "react"
+import {useHistory} from 'react-router-dom'
 import { useSelector } from "react-redux"
 
 const OtherVillages = () => {
   const { otherVillages } = useSelector(({ villageReducer }) => villageReducer)
+  const {push} = useHistory()
   return (
     <div>
       <div>OtherVillages:</div>
--- src/Components/Villages.js
+++ src/Components/Villages.js
@@ -1,15 +1,20 @@
 import React from "react"
+import {useHistory} from 'react-router-dom'
 import { useSelector, connect } from "react-redux"
 import { setVillage } from "../redux/villageReducer"
 
 const Villages = ({ setVillage }) => {
   const {villages} = useSelector(({ villageReducer }) => villageReducer)
+  const {push} = useHistory()
   return (
     <div>
       <div>Villages:</div>
       {villages &&
         villages.map(({ village_name, village_id }, index) => (
-          <div key={village_id} onClick={() => setVillage(villages[index])}>
+          <div key={village_id} onClick={() => {
+            setVillage(villages[index])
+            push('/dashboard/village')
+            }}>
             {village_name}
           </div>
         ))}
